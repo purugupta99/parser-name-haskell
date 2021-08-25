ce0703cbe0a3d801fe1a4d1a08d6376a76283673
Paritosh Raj <paritosh@hasura.io>
2021-08-25 19:57:44 +0530

    parse according to spec

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, unpack)
 import Data.Void
 import Control.Applicative hiding (many, some)
 import Control.Monad.IO.Class
@@ -10,73 +10,113 @@ import Text.Megaparsec
 import Text.Megaparsec.Char as M
 import Text.Megaparsec.Char.Lexer as L
 
+{-
+> an uppercase (resp. lowercase) letter is a character in the 26-character 
+  uppercase (resp. lowercase) Latin alphabet used to write English
+
+> a name unit is an uppercase letter followed by zero or more lowercase 
+  letters
+
+> a name is one or more name units separated by hyphens
+
+> a full name consists of names separated by spaces: a first name, then 
+  an optional middle name, and finally a last name
+-}
+
+
 inp = ["Simon King", "Simon King-Kong", "Simon-Sequeira Vinson King-Lord"]
 
-data Name = Name {
-      fName :: Text
-    , mName :: Maybe Text
-    , lName :: Text
-} deriving (Show)
+newtype NameUnit = NameUnit Text
+newtype NameB = NameB [NameUnit]
+
+instance Show NameB where
+    show (NameB []) = []
+    show (NameB [a]) = show a
+    show (NameB (a:lst)) = show a ++ "-" ++ show (NameB lst)
+
+instance Show NameUnit where
+    show (NameUnit a) = unpack a
+
+instance Show NameBlock where
+    show Empty = ""
+    show name = show (nameB name) ++  " " ++ show (restName name)
 
 data NameBlock = NameBlock {
-    nameB :: Text
+    nameB :: NameB
   , restName :: NameBlock
-} | Empty deriving (Show)
+} | Empty
+
+data Name = Name {
+      fName :: NameB
+    , mName :: NameB
+    , lName :: NameB
+} deriving (Show)
+
+me = Name {
+    fName = NameB [NameUnit "Paritosh", NameUnit "Kong"],
+    mName = NameB [],
+    lName = NameB [NameUnit "Raj"]
+}
 
 sKing = NameBlock {
-    nameB = "Simon"
+    nameB = NameB [NameUnit "Paritosh", NameUnit "Kong"]
     , restName = NameBlock {
-        nameB = "King"
+        nameB = NameB [NameUnit "Raj"]
         , restName = Empty
     }
 }
 
 parseNameBlock :: Parser NameBlock
 parseNameBlock = do
-    x <- parseString <* spaceConsumer
-    if null x
+    x <- parseNameB <* spaceConsumer
+    if checkNull x
         then pure Empty
         else do
             y <- parseNameBlock
             pure $ NameBlock {
-                    nameB = pack x
+                    nameB = x
                     , restName = y
                 }
 
 
-tmpParsed = Name {
-      fName = "Simon"
-    , mName = Nothing
-    , lName = "King"
-}
+
 
 type Parser a = Parsec Void Text a
 
 spaceConsumer :: Parser ()
 spaceConsumer = M.space
 
-parseString :: Parser String
-parseString = many (oneOf (['a'..'z'] ++ ['A'..'Z'] ++ ['-']) :: Parser Char)
+parseNameUnit :: Parser NameUnit
+parseNameUnit = do
+    -- first char is capital alphabet
+    c1 <- M.upperChar
+    rest <- many M.lowerChar
+    return $ NameUnit $ pack (c1 : rest)
 
--- parseTest (satisfy (oneOf ['a'..'z']) :: Parser Char) "a"
+parseChar :: Char -> Parser Text
+parseChar c = do
+    c' <- M.char c
+    return $ pack [c']
+
+parseNameB :: Parser NameB
+parseNameB = do
+    -- a <- many (parseChar '-' >> parseNameUnit)
+    a <- parseNameUnit `sepBy` parseChar '-'
+    return $ NameB a
+
+checkNull :: NameB -> Bool
+checkNull (NameB a) = null a
 
 parseName :: Parser Name
 parseName = do
-    -- fN <- parseString <* spaceConsumer
-    -- mN <- parseString <* spaceConsumer
-    -- lN <- parseString 
-
-    fN <- parseString <* spaceConsumer
-    mN <- parseString <* spaceConsumer
-    lN <- parseString
-
-    let
-        temp = parseMaybe spaceConsumer
+    fN <- parseNameB <* spaceConsumer
+    mN <- parseNameB <* spaceConsumer
+    lN <- parseNameB
 
     pure (Name {
-        fName = pack fN
-        , mName = if null lN then Nothing else Just $ pack mN
-        , lName = if null lN then pack mN else pack lN
+        fName = fN
+        , mName = if checkNull lN then NameB [] else mN
+        , lName = if checkNull lN then mN else lN
     })
 
 main :: IO ()
